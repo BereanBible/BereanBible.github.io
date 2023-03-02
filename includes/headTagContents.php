3a716f0d7490a939089ace585bde2b0cfd71ad04
Cahleeb <calebchristopherparks@gmail.com>
2023-03-02 00:59:18 -0500

    Starting out
    Started setting up main page layout and css, and service worker.

---
+++ includes/headTagContents.php
@@ -0,0 +1,10 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Stylesheets -->
+<link href="css/style.css" rel="stylesheet" />    
+
+<!-- Favicon -->
+<link rel="icon" href="graphics/favicon.ico" type="image/x-icon">
+
+<link rel="manifest" href="manifest.json">
