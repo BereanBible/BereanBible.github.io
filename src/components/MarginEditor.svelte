46fa9a7e415c8a9e09137213898a4d08cb01519f
Cahleeb <calebchristopherparks@gmail.com>
2023-08-22 10:10:01 -0400

    Moved Bible, margin, and interlinear content out into components.

---
+++ src/components/MarginEditor.svelte
@@ -0,0 +1,163 @@
+<!--
+@component
+The Margin component.
+-->
+<script>
+    export let marginBackground, defaultTextColor, defaultFont;
+    console.log(
+        "Margin props:",
+        marginBackground,
+        defaultTextColor,
+        defaultFont
+    );
+</script>
+
+<!--HTML-->
+
+<div
+    id="marginContent"
+    style="
+        --MarginBackground: {marginBackground};
+        --defaultTextColor: {defaultTextColor};
+        --defaultFont: {defaultFont};
+        
+        margin: 0px;
+		padding: 0px;
+        background-color: {marginBackground};
+        color: {defaultTextColor};
+
+        display: flex;
+        flex-direction: column;
+    "
+>
+    <p style="padding: 0px; margin: 0px;">
+        The Gospel’s Foolishness [Ch 1:18-30] <br />
+        <br /> - The Gospel is foolish to the unsaved [Ch 1:18-25]
+        <br /> - The gospel is foolishness to the unbeliever but to those being
+        saved is is the power of God (1:18)
+        <br /> - Quotes God’s Isaiah 29:14 promise to destroy/thwart the wisdom
+        of the wise (1:18)
+        <br /> - God has made the world’s wisdom foolish (1:20)
+        <br /> - Because the world in its wisdom did not come to know God (in
+        God’s wisdom this occurred) God was pleased to save those who believe
+        through the foolishness of the Gospel (1:21)
+        <br /> - “Since God in his wisdom saw to it that the world would never
+        know him through human wisdom, he has used our foolish preaching to save
+        those who believe.”
+        <br /> - The Gospel is offensive to the Jews (who want signs), and
+        nonsense to the Greeks (who seek philosophy). (1:22-23)
+        <br /> - However, to the Elect—Jew or greek—Christ is the power and
+        wisdom of God (1:24)
+        <br /> - God is totally wiser and stronger than men (1:25)
+
+        <br /> - Using the lowly to shame the world's greatest [Ch 1:26-30]
+        <br /> - The Corinthians God called were not wise, mighty, or noble
+        (1:26)
+        <br /> - God chose to use foolish and weak people to shame the worldly
+        strong and the worldly wise, and to use what the world despises and
+        considers nothing to bring down things the world considers important
+        (1:27-28)
+        <br /> - All so that no one may boast before God (1:29)
+        <br /> - Through Christ, God imparts wisdom, righteousness,
+        sanctification, and redemption to us (1:30)
+        <br /> - Boast only about the Lord (1:31)
+    </p>
+    <p style="padding: 0px; margin: 0px;">
+        The Gospel’s Foolishness [Ch 1:18-30] <br />
+        <br /> - The Gospel is foolish to the unsaved [Ch 1:18-25]
+        <br /> - The gospel is foolishness to the unbeliever but to those being
+        saved is is the power of God (1:18)
+        <br /> - Quotes God’s Isaiah 29:14 promise to destroy/thwart the wisdom
+        of the wise (1:18)
+        <br /> - God has made the world’s wisdom foolish (1:20)
+        <br /> - Because the world in its wisdom did not come to know God (in
+        God’s wisdom this occurred) God was pleased to save those who believe
+        through the foolishness of the Gospel (1:21) <br /> - “Since God in his
+        wisdom saw to it that the world would never know him through human
+        wisdom, he has used our foolish preaching to save those who believe.”
+        <br /> - The Gospel is offensive to the Jews (who want signs), and
+        nonsense to the Greeks (who seek philosophy). (1:22-23)
+        <br /> - However, to the Elect—Jew or greek—Christ is the power and
+        wisdom of God (1:24)
+        <br /> - God is totally wiser and stronger than men (1:25)
+
+        <br /> - Using the lowly to shame the world's greatest [Ch 1:26-30]
+        <br /> - The Corinthians God called were not wise, mighty, or noble
+        (1:26)
+        <br /> - God chose to use foolish and weak people to shame the worldly
+        strong and the worldly wise, and to use what the world despises and
+        considers nothing to bring down things the world considers important
+        (1:27-28) <br /> - All so that no one may boast before God (1:29)
+        <br /> - Through Christ, God imparts wisdom, righteousness,
+        sanctification, and redemption to us (1:30)
+        <br /> - Boast only about the Lord (1:31)
+    </p>
+    <p style="padding: 0px; margin: 0px;">
+        The Gospel’s Foolishness [Ch 1:18-30] <br />
+        <br /> - The Gospel is foolish to the unsaved [Ch 1:18-25]
+        <br /> - The gospel is foolishness to the unbeliever but to those being
+        saved is is the power of God (1:18)
+        <br /> - Quotes God’s Isaiah 29:14 promise to destroy/thwart the wisdom
+        of the wise (1:18)
+        <br /> - God has made the world’s wisdom foolish (1:20)
+        <br /> - Because the world in its wisdom did not come to know God (in
+        God’s wisdom this occurred) God was pleased to save those who believe
+        through the foolishness of the Gospel (1:21) <br /> - “Since God in his
+        wisdom saw to it that the world would never know him through human
+        wisdom, he has used our foolish preaching to save those who believe.”
+        <br /> - The Gospel is offensive to the Jews (who want signs), and
+        nonsense to the Greeks (who seek philosophy). (1:22-23)
+        <br /> - However, to the Elect—Jew or greek—Christ is the power and
+        wisdom of God (1:24)
+        <br /> - God is totally wiser and stronger than men (1:25)
+
+        <br /> - Using the lowly to shame the world's greatest [Ch 1:26-30]
+        <br /> - The Corinthians God called were not wise, mighty, or noble
+        (1:26)
+        <br /> - God chose to use foolish and weak people to shame the worldly
+        strong and the worldly wise, and to use what the world despises and
+        considers nothing to bring down things the world considers important
+        (1:27-28)
+        <br /> - All so that no one may boast before God (1:29)
+        <br /> - Through Christ, God imparts wisdom, righteousness,
+        sanctification, and redemption to us (1:30)
+        <br /> - Boast only about the Lord (1:31)
+    </p>
+    <p style="padding: 0px; margin: 0px;">
+        The Gospel’s Foolishness [Ch 1:18-30] <br />
+        <br /> - The Gospel is foolish to the unsaved [Ch 1:18-25]
+        <br /> - The gospel is foolishness to the unbeliever but to those being
+        saved is is the power of God (1:18)
+        <br /> - Quotes God’s Isaiah 29:14 promise to destroy/thwart the wisdom
+        of the wise (1:18)
+        <br /> - God has made the world’s wisdom foolish (1:20)
+        <br /> - Because the world in its wisdom did not come to know God (in
+        God’s wisdom this occurred) God was pleased to save those who believe
+        through the foolishness of the Gospel (1:21)
+        <br /> - “Since God in his wisdom saw to it that the world would never
+        know him through human wisdom, he has used our foolish preaching to save
+        those who believe.”
+        <br /> - The Gospel is offensive to the Jews (who want signs), and
+        nonsense to the Greeks (who seek philosophy). (1:22-23)
+        <br /> - However, to the Elect—Jew or greek—Christ is the power and
+        wisdom of God (1:24)
+        <br /> - God is totally wiser and stronger than men (1:25)
+
+        <br /> - Using the lowly to shame the world's greatest [Ch 1:26-30]
+        <br /> - The Corinthians God called were not wise, mighty, or noble
+        (1:26)
+        <br /> - God chose to use foolish and weak people to shame the worldly
+        strong and the worldly wise, and to use what the world despises and
+        considers nothing to bring down things the world considers important
+        (1:27-28)
+        <br /> - All so that no one may boast before God (1:29)
+        <br /> - Through Christ, God imparts wisdom, righteousness,
+        sanctification, and redemption to us (1:30)
+        <br /> - Boast only about the Lord (1:31)
+    </p>
+</div>
+
+<!--CSS-->
+
+<style>
+</style>
